95d672607f03ff830e90baa30c02d44031e7f275
Alex <alexlipskyi@icloud.com>
2023-08-06 13:36:50 +0300

    test5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-new SimpleLightbox(".gallery a");
+
+axios.defaults.headers.common["x-api-ley"] = '38590666-b4e4facc0390580085af70521';
+axios.baseUrl = "https://pixabay.com/api/";
+
 
 
 // refs
-const refs = {
-    
+const refs = { 
     KEY: '38590666-b4e4facc0390580085af70521',
     baseUrl: 'https://pixabay.com/api/',
     failureMessage:  "Sorry, there are no images matching your search query. Please try again.",
@@ -23,16 +26,7 @@ const refs = {
 
 refs.form.addEventListener("submit", handleSubmit);
 refs.loadMore.addEventListener("click",handleSubmit);
-refs.gallery.addEventListener("click", showImage);
 
-function showImage (e) {
-  
-  const clickedElement = e.target.closest('a');
-  if (!clickedElement) return;
-
-  gallery.open(clickedElement.getAttribute('href'));
-}
-   
 // Sumblit Handle
 
  async function handleSubmit(e) {
@@ -43,31 +37,30 @@ function showImage (e) {
       refs.page = 1;
       refs.gallery.textContent = "";
     }
+
    try {
 
-   const result = await fetchPhotos(refs.form.searchQuery.value);
-   
+   const result = await fetchPhotos(refs.form.searchQuery.value);   
     refs.totalPages = (result.totalHits/refs.limit);   // Check for the last page
      
     if(Math.ceil(refs.totalPages) < refs.page && result.hits.length > 0) {
       refs.loadMore.classList.add("hidden");  
       return Notiflix.Notify.failure(refs.limitMessage);
      }
+
     if(result.hits.length===0) {
       return Notiflix.Notify.warning(refs.failureMessage);
- 
      } 
    
     if(refs.page ===1) {
-
       Notiflix.Notify.info(`Hooray! We found ${result.totalHits} images.`);
     }
          
+  
       renderMarkup(result.hits);
       refs.page+=1;
       refs.loadMore.classList.remove("hidden");
-
-
+        
   }  catch (error) {
 
     Notiflix.Notify.failure(error)
@@ -91,7 +84,7 @@ function renderMarkup(images) {
         const markup = images.reduce((html,{webformatURL,largeImageURL,tags,likes,views,comments,downloads} ) => {
          return html+` 
          <div class="photo-card">
-         <a href="${largeImageURL}">
+         <a class="gallery__link" href="${largeImageURL}">
          <img src="${webformatURL}" alt="${tags}" width="300px" loading="lazy" />
          </a>
          <div class="info">
@@ -113,6 +106,6 @@ function renderMarkup(images) {
      },"")   
 
      refs.gallery.insertAdjacentHTML("beforeend",markup);
+     new SimpleLightbox(".gallery a").refresh()
 }
-
 
